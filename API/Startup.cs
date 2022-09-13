e5f664fd306a04325390678862b7d2b85fbdeed3
Max Hjärpe <hjarpemax@gmail.com>
2022-09-13 14:26:39 +0200

    refactor api call to a generic repository pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entity.Interfaces;
 using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +29,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<ICourseRepository, CourseRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
             {
