38f32daa34d51c4cf6256cad663a8bb168aace37
Max Hjärpe <hjarpemax@gmail.com>
2022-09-15 09:50:40 +0200

    setup generic repository

--- API/Controllers/CoursesController.cs
+++ API/Controllers/CoursesController.cs
@@ -13,26 +13,25 @@ namespace API.Controllers
 {
     public class CoursesController : BaseController
     {
-        private readonly ICourseRepository _repository;
         private readonly IMapper _mapper;
-        public CoursesController(ICourseRepository repository, IMapper mapper)
+        private readonly IGenericRepository<Course> _repository;
+        public CoursesController(IGenericRepository<Course> repository, IMapper mapper)
         {
-            _mapper = mapper;
             _repository = repository;
-
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<CourseDto>>> GetCourse()
         {
-            var courses = await _repository.GetCoursesAsync();
+            var courses = await _repository.ListAllAsync();
             return Ok(_mapper.Map<IReadOnlyList<Course>, IReadOnlyList<CourseDto>>(courses));
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<CourseDto>> GetCourse(Guid id)
         {
-            var course = await _repository.GetCourseByIdAsync(id);
+            var course = await _repository.GetByIdAsync(id);
 
             return _mapper.Map<Course, CourseDto>(course);
         }
---
+++ Infrastructure/GenericRepository.cs
@@ -0,0 +1,27 @@
+using Entity.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly StoreContext _context;
+        public GenericRepository(StoreContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<T> GetByIdAsync(dynamic id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+    }
+}
