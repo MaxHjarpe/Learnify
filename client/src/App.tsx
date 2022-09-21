7014821587d39a7778322bb9db3f52077c0dd803
Max Hjärpe <hjarpemax@gmail.com>
2022-09-21 13:13:41 +0200

    setup fetching of basket on application start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import DetailPage from "./pages/DetailPage";
@@ -9,12 +9,37 @@ import Category from "./components/Categories";
 import CategoryPage from "./pages/CategoryPage";
 import DescriptionPage from "./pages/DescriptionPage";
 import BasketPage from "./pages/BasketPage";
+import { useStoreContext } from "./context/StoreContext";
+import agent from "./actions/agent";
 
 function App() {
+  const { setBasket } = useStoreContext();
+
+  function getCookie(name: string) {
+    return (
+      document.cookie.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)")?.pop() ||
+      ""
+    );
+  }
+
+  useEffect(() => {
+    const clientId = getCookie("clientId");
+
+    if (clientId) {
+      agent.Baskets.get()
+        .then((response) => {
+          setBasket(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [setBasket]);
+
   return (
     <>
-    <Navigation/>
-    <Route exact path="/" component={Category}/>
+      <Navigation />
+      <Route exact path="/" component={Category} />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/category/:id" component={CategoryPage} />
