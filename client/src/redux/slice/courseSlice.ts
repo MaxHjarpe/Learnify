c7baa13e259dfb91c8b3d7d31a461bd63fce1569
Max Hjärpe <hjarpemax@gmail.com>
2022-09-21 19:08:21 +0200

    begin setup of course slice and entity adapters

---
+++ client/src/redux/slice/courseSlice.ts
@@ -0,0 +1,42 @@
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit";
+import agent from "../../actions/agent";
+import { Course } from "../../models/course";
+import { PaginatedCourse } from "../../models/paginatedCourse";
+
+const coursesAdapter = createEntityAdapter<Course>();
+
+const getCoursesAsync = createAsyncThunk<PaginatedCourse | undefined, void>(
+  "course/getCoursesAsync",
+  async () => {
+    try {
+      return await agent.Courses.list();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const courseSlice = createSlice({
+  name: "course",
+  initialState: coursesAdapter.getInitialState({
+    coursesLoaded: false,
+    status: "idle",
+  }),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCoursesAsync.pending, (state) => {
+      state.status = "pending";
+    });
+    builder.addCase(getCoursesAsync.fulfilled, (state, action) => {
+      coursesAdapter.setMany(state, action.payload!.data);
+      state.status = "idle";
+    });
+    builder.addCase(getCoursesAsync.rejected, (state) => {
+      state.status = "idle";
+    });
+  },
+});
--- client/src/redux/store/configureStore.ts
+++ client/src/redux/store/configureStore.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import useSelection from "antd/lib/table/hooks/useSelection";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { basketSlice } from "../slice/basketSlice";
+import { courseSlice } from "../slice/courseSlice";
 import { loginSlice } from "../slice/loginSlice";
 
 
@@ -9,6 +10,7 @@ export const store = configureStore({
     reducer: {
         login: loginSlice.reducer,
         basket: basketSlice.reducer,
+        course: courseSlice.reducer,
     },
 });
 
@@ -16,5 +18,5 @@ export type RootState =  ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
