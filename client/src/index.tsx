40eaa6557325031ad2a51809cf025773c0713493
Max Hjärpe <hjarpemax@gmail.com>
2022-09-21 17:30:34 +0200

    install and setup redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,11 +5,18 @@ import "./sass/main.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { StoreProvider } from "./context/StoreContext";
+import { configureStore } from "./redux/store/configureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore();
 
 ReactDOM.render(
   <Router>
     <StoreProvider>
+      <Provider store={store}>
       <App />
+      </Provider>
+
     </StoreProvider>
   </Router>,
   document.getElementById("root")
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { LoginState } from "../redux/loginReducer";
 
 const LoginPage = () => {
-    return <h1>LoginPage</h1>
+
+    const {visits} = useSelector((state: LoginState) => state);
+
+
+    return <h1>Number of visits: {visits}</h1>
 };
 
 export default LoginPage;
