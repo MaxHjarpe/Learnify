5f720b805788bf4b355e608b46063cd85f7cf04d
Max Hjärpe <hjarpemax@gmail.com>
2022-09-21 12:07:48 +0200

    add styles and structure to basket page. setup functionality

--- client/src/pages/BasketPage.tsx
+++ client/src/pages/BasketPage.tsx
@@ -1,20 +1,72 @@
 import React, { useEffect, useState } from "react";
+import { Table } from "antd";
 import agent from "../actions/agent";
-import { Basket } from "../models/Basket";
+import { Basket, CourseItem } from "../models/Basket";
+import * as FaIcons from "react-icons/fa";
 
 const BasketPage = () => {
-  const [items, setItems] = useState<Basket>();
+  const [items, setItems] = useState<Basket | null>();
 
   useEffect(() => {
     agent.Baskets.get().then((response) => {
-      setItems(response);
+      newData(response);
     });
   }, []);
 
+  const newData = (items: Basket | null) => {
+    items?.items.map((item: CourseItem, index: number) =>
+      Object.assign(item, { key: index })
+    );
+
+    setItems(items);
+  };
+
+  const removeItem = (courseId: string) => {
+    agent.Baskets.removeItem(courseId).catch((error) => {
+      console.log(error);
+    });
+  };
+
+  const columns = [
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+      render: (url: string) => {
+        return <img width="100px" src={url} alt={url} />;
+      },
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Instructor",
+      key: "instructor",
+      dataIndex: "instructor",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (item: CourseItem) => (
+        <div onClick={() => removeItem(item.courseId)}>
+          <FaIcons.FaTrash />
+        </div>
+      ),
+    },
+  ];
+
   return (
-    <>
-      <h1>{items?.clientId}</h1>
-    </>
+    <div className="basket-page">
+      <h1 className="basket-page__header">Shopping Cart</h1>
+      <Table columns={columns} dataSource={items?.items} />
+    </div>
   );
 };
 
