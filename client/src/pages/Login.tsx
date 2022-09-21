76a5f1cadacd920ae42c3487eb3b61f879db67a6
Max Hjärpe <hjarpemax@gmail.com>
2022-09-21 17:44:43 +0200

    test dispatching actions to reducer

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,13 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { LoginState } from "../redux/loginReducer";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, LoginState } from "../redux/loginReducer";
 
 const LoginPage = () => {
+  const { visits } = useSelector((state: LoginState) => state);
 
-    const {visits} = useSelector((state: LoginState) => state);
+  const dispatch = useDispatch();
 
 
-    return <h1>Number of visits: {visits}</h1>
+  return (
+    <>
+      <h1>Number of visits: {visits}</h1>
+      <button onClick={() => dispatch(increment(5))}>increment</button>
+    </>
+  );
 };
 
 export default LoginPage;
--- client/src/redux/loginReducer.ts
+++ client/src/redux/loginReducer.ts
@@ -1,13 +1,28 @@
+export const UPDATE_VISIT = "UPDATE_VISIT";
+
 export interface LoginState {
-    visits: number;
+  visits: number;
 }
 
-
 export const initialState: LoginState = {
-    visits: 1,
+  visits: 1,
 };
 
+export function increment(amount = 1) {
+  return {
+    type: UPDATE_VISIT,
+    payload: amount,
+  };
+}
 
 export default function loginReducer(state = initialState, action: any) {
-    return state;
+  switch (action.type) {
+    case UPDATE_VISIT:
+      return {
+        ...state,
+        visits: state.visits + action.payload,
+      };
+    default:
+      return state;
+  }
 }
