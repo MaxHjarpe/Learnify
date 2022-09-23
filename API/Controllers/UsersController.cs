83395538328f6840f79ab900ac39235d1c054cb4
Max Hjärpe <hjarpemax@gmail.com>
2022-09-23 14:20:52 +0200

    add registerDto, create register function

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -19,10 +19,32 @@ namespace API.Controllers
         public async Task<ActionResult<User>> Login(LoginDto loginDto)
         {
             var user = await _userManager.FindByEmailAsync(loginDto.Email);
-            if (user == null && !await _userManager.CheckPasswordAsync(user, loginDto.Password)) {
+            if (user == null && !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+            {
                 return Unauthorized(new ApiResponse(401));
             }
             return user;
         }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(RegisterDto registerDto)
+        {
+            var user = new User { UserName = registerDto.Username, Email = registerDto.Email };
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return ValidationProblem();
+            }
+
+            await _userManager.AddToRoleAsync(user, "Student");
+            return user;
+        }
+
+
     }
 }
