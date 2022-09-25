66b1b2e17107d44b405a16f6a08bace47e31761f
Max Hjärpe <hjarpemax@gmail.com>
2022-09-25 12:59:36 +0200

    add stripe properties to Dto

--- API/Dto/BasketDto.cs
+++ API/Dto/BasketDto.cs
@@ -7,5 +7,9 @@ namespace API.Dto
         public string ClientId { get; set; }
 
         public List<BasketItemDto> Items { get; set; }
+
+        public string PaymentIntentId { get; set; }
+
+        public string ClientSecret { get; set; }
     }
 }
