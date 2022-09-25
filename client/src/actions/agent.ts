2b2edd3b77d57cddfc10cc5acb11795b09c4d2e8
Max Hjärpe <hjarpemax@gmail.com>
2022-09-25 14:27:07 +0200

    add payments, paymentIntent and axios interceptor

--- client/src/actions/agent.ts
+++ client/src/actions/agent.ts
@@ -4,12 +4,21 @@ import { Category } from "../models/category";
 import { Course } from "../models/course";
 import { Basket } from "../models/basket";
 import { Login, Register, User } from "../models/user";
+import { Store } from "redux";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 axios.defaults.withCredentials = true;
+
+export const axiosInterceptor = (store: Store) => {
+  axios.interceptors.request.use((config) => {
+    const token = store.getState().user.user?.token;
+    if (token) config.headers!.Authorization = `Bearer ${token}`;
+    return config;
+  });
+};
 
 const requests = {
   get: <T>(url: string, params?: URLSearchParams) =>
@@ -43,11 +52,16 @@ const Baskets = {
   removeItem: (courseId: string) => requests.del(`basket?courseId=${courseId}`),
 };
 
+const Payments = {
+  paymentIntent: () => requests.post<Basket>("payments", {}),
+};
+
 const agent = {
   Courses,
   Categories,
   Baskets,
   Users,
+  Payments,
 };
 
 export default agent;
