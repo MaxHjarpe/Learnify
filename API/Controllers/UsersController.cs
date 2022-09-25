1b6695b1f5f68a407c36fd03f940425ed758a951
Max Hjärpe <hjarpemax@gmail.com>
2022-09-25 16:10:59 +0200

    implement a current user endpoint to return courses purchased

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -42,6 +42,7 @@ namespace API.Controllers
             var userBasket = await ExtractBasket(user.UserName);
 
             var basket = await ExtractBasket(Request.Cookies["clientId"]);
+            var courses = _context.UserCourses.AsQueryable();
 
             if (basket != null)
             {
@@ -57,6 +58,7 @@ namespace API.Controllers
                 Email = user.Email,
                 Token = await _tokenService.GenerateToken(user),
                 Basket = basket != null ? _mapper.Map<Basket, BasketDto>(basket) : _mapper.Map<Basket, BasketDto>(userBasket),
+                Courses = courses.Where(x => x.UserId == user.Id).Select(u => u.Course).ToList(),
             };
         }
 
@@ -103,11 +105,29 @@ namespace API.Controllers
             }
 
             var result = await _context.SaveChangesAsync() > 0;
-            
+
             if (result) return Ok();
 
             return BadRequest(new ApiResponse(400, "Problem adding courses"));
         }
+
+        [Authorize]
+        [HttpGet("currentUser")]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByIdAsync(User.Identity.Name);
+            var basket = await ExtractBasket(User.Identity.Name);
+            var courses = _context.UserCourses.AsQueryable();
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateToken(user),
+                Basket = _mapper.Map<Basket, BasketDto>(basket),
+                Courses = courses.Where(x => x.UserId == user.Id).Select(u => u.Course).ToList(),
+            };
+        }
+
 
         private async Task<Basket> ExtractBasket(string clientId)
         {
--- client/src/actions/agent.ts
+++ client/src/actions/agent.ts
@@ -33,6 +33,7 @@ const Users = {
   login: (values: Login) => requests.post<User>("users/login", values),
   register: (values: Register) => requests.post<User>("users/register", values),
   addCourse: () => requests.post("users/purchaseCourses", {}),
+  currentUser: () => requests.get<User>("user/currentUser"),
 };
 
 const Courses = {
