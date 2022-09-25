7ee0b8cccb8f7ff9863b0cebf88aab1bff7860b9
Max Hjärpe <hjarpemax@gmail.com>
2022-09-25 14:30:37 +0200

    implement axios interceptor to check if user has a token

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,8 +7,10 @@ import reportWebVitals from "./reportWebVitals";
 import { StoreProvider } from "./context/StoreContext";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/configureStore";
+import { axiosInterceptor } from "./actions/agent";
 
 
+axiosInterceptor(store);
 
 ReactDOM.render(
   <Router>
