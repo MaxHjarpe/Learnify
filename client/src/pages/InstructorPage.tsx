a27c477b641c8964290c2cae91b7447b7c80d04a
Max Hjärpe <hjarpemax@gmail.com>
2022-09-28 09:31:01 +0200

    fleshing out instructor page and styling it

--- client/src/pages/InstructorPage.tsx
+++ client/src/pages/InstructorPage.tsx
@@ -1,10 +1,56 @@
+import { Button, Card } from "antd";
+import Meta from "antd/lib/card/Meta";
+import { useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Course } from "../models/course";
+import { getUnpublishedCourses } from "../redux/slice/userSlice";
+import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
+
 const InstructorPage = () => {
-    return (
-        <div className="instructor">
-            <div className="instructor__left"></div>
-            <div className="instructor__right"></div>
+  const dispatch = useAppDispatch();
+  const history = useHistory();
+  useEffect(() => {
+    dispatch(getUnpublishedCourses());
+  }, [dispatch]);
+
+  const { unpublishedCourses } = useAppSelector((state) => state.user);
+
+  const makeCourse = () => {
+    history.push("instructor/course");
+  };
+
+  return (
+    <div className="instructor">
+      <div className="instructor__left">
+        <div className="instructor__left__header">
+          {unpublishedCourses.length > 0
+            ? `My Unpublished Courses`
+            : `Create a new Course`}
         </div>
-    )
-}
+        <div className="instructor__left__courses">
+          {unpublishedCourses.map((course: Course, index: number) => {
+            return (
+              <Link key={index} to={`${course.id}/lectures`}>
+                <Card
+                  hoverable
+                  style={{ width: 240 }}
+                  cover={<img src={course.image} alt={course.title} />}
+                >
+                  <Meta
+                    title={course.title}
+                    description={course.subTitle}
+                  ></Meta>
+                </Card>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+      <div className="instructor__right">
+        <Button type="primary" onClick={makeCourse}>New Course</Button>
+      </div>
+    </div>
+  );
+};
 
 export default InstructorPage;
