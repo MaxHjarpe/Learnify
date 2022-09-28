b16e9a6e8360e117bcb95298d7efdf0bdc8c9a87
Max Hjärpe <hjarpemax@gmail.com>
2022-09-28 14:48:17 +0200

    unioned startup and program class, as of dotnet 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,49 +1,120 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Entity;
-using Infrastructure;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace API
+
+var builder = WebApplication.CreateBuilder(args);
+
+//add services
+
+builder.Services.AddIdentityCore<User>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+            }
+            )
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<StoreContext>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(opt =>
 {
-    public class Program
+    opt.TokenValidationParameters = new TokenValidationParameters
     {
-        public static async Task Main(string[] args)
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:TokenKey"]))
+    };
+});
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<TokenService>();
+builder.Services.AddScoped<PaymentService>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StoreContext>(x =>
+{
+    x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"),
+    x => x.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
+});
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+        .WithOrigins("http://localhost:3000");
+    });
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+});
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = actionContext =>
+    {
+        var errors = actionContext.ModelState
+        .Where(e => e.Value.Errors.Count > 0)
+        .SelectMany(x => x.Value.Errors)
+        .Select(x => x.ErrorMessage).ToArray();
+
+        var errorResponse = new ApiValidationErrorResponse
         {
-            var host = CreateHostBuilder(args).Build();
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-            var logger = services.GetRequiredService<ILogger<Program>>();
+            Errors = errors
+        };
+        return new BadRequestObjectResult(errorResponse);
+    };
+});
 
-            try
-            {
-                var userManager = services.GetRequiredService<UserManager<User>>();
-                var context = services.GetRequiredService<StoreContext>();
-                await context.Database.MigrateAsync();
-                await StoreContextSeed.SeedAsync(context, logger, userManager);
+//HTTP request pipeline
 
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Something wrong happened during migration");
-            }
+var app = builder.Build();
 
-            await host.RunAsync();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+app.UseMiddleware<ExceptionMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
 }
+
+app.UseStatusCodePagesWithReExecute("/redirect/{0}");
+
+// app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context, logger, userManager);
+
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "Something wrong happened during migration");
+}
+
+await app.RunAsync();
+
+
