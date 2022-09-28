d2f4fe0531ee19ebd27c007c89659f6c43dde032
Max Hjärpe <hjarpemax@gmail.com>
2022-09-28 11:35:11 +0200

    changed import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import LoginPage from "./pages/Login";
 import DetailPage from "./pages/DetailPage";
 import Homepage from "./pages/Homepage";
 import Navigation from "./components/Navigation";
-import "antd/dist/antd.css";
+import "antd/dist/antd.min.css";
 import Category from "./components/Categories";
 import CategoryPage from "./pages/CategoryPage";
 import DescriptionPage from "./pages/DescriptionPage";
