faf5f248629ec41a0dced1dcb5559140adf0c9b8
Max Hjärpe <hjarpemax@gmail.com>
2022-10-02 12:37:15 +0200

    footer styling changes

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,18 +4,18 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div className="footer">
+      <footer className="footer">
+        <div >
           <div className="footer__header">
-            <h2>Name</h2>
+            <h3>Name</h3>
             <div className="footer__header__item">
               <p>Max Hjärpe</p>
             </div>
           </div>
           <div className="footer__header">
-            <h2>Email</h2>
+            <h3>Email</h3>
             <div className="footer__header__item">
-              <p>hjarpemax@gmail.com</p>
+              <p >hjarpemax@gmail.com</p>
             </div>
           </div>
           <div className="footer__icon__container">
